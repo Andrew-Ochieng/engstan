d0a5705ac97fbb546674f890f8f560bbfb3b07eb
Code-X <andrewomosh44@gmail.com>
2023-03-08 15:00:26 +0300

    add usefetch- custom hook

--- src/components/UseFetch.js
+++ src/components/UseFetch.js
@@ -1,9 +1,33 @@
-const UseFetch = () => {
-    return ( 
-        <>
-            
-        </>
-     );
+import { useEffect, useState } from "react"
+
+const UseFetch = (url) => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetch(url)
+           .then((response) => {
+                if (!response.ok) {
+                    throw Error('Could not fetch data from the resource!')
+                }
+                return response.json()
+            })
+           .then((data) => {
+                console.log(data)
+                setData(data)
+                setLoading(false)
+                setError(null)
+           })
+           .catch((err) => {
+                setLoading(false)
+                setError(err.message)
+           })
+    }, [url])
+
+
+
+    return ({data, loading, error});
 }
  
 export default UseFetch;
