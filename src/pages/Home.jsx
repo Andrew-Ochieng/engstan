447537ae864b16fa935b9418e0d5fbef010a252b
Code-X <andrewomosh44@gmail.com>
2023-09-16 09:29:54 +0300

    build: add spinner to home-prducts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Home/Hero";
 import Reachout from "../components/Home/Reachout";
 import Testimonials from "../components/Home/Testimonials";
 import ProductList from "../components/Products/ProductList";
+import { GridLoader } from "react-spinners";
 
 const Home = ({products, loading}) => {
   return ( 
@@ -13,7 +14,15 @@ const Home = ({products, loading}) => {
             <h2 className="content-title text-center">Our Products</h2>
             <div>
               {loading ? (
-                <div className="h-32">Loading Prodcts...</div>
+                <div className="flex items-center justify-center md:my-48 my-24">
+                  <GridLoader 
+                      color="#084E7A" 
+                      loading={loading}
+                      size={20}
+                      aria-label="Loading Content..."
+                      data-testid="loader"
+                  />
+                </div>
               ) : (
                 <ProductList products={products.slice(0,8)  }/>
               )}
